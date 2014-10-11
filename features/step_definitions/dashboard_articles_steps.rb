cfabf05060e9b875b258c684ac40d78743c306db
Michał Skóra <mskora@fractalsoft.org>
2014-10-11 18:23:53 +0200

    Some refactoring

--- features/step_definitions/dashboard_articles_steps.rb
+++ features/step_definitions/dashboard_articles_steps.rb
@@ -1,13 +1,17 @@
+Before do
+  @params = { title: 'Foobar', body: 'Lorem ipsum' }
+end
+
 When(/^I create new article$/) do
   click_link 'New Article'
-  fill_in :article_title, with: 'Foobar'
-  fill_in :article_body, with: 'Lorem ipsum'
+  fill_in :article_title, with: @params[:title]
+  fill_in :article_body, with: @params[:body]
   click_button 'Create Article'
 end
 
 Then(/^I should see it at index page$/) do
   within('.article') do
-    expect(find('.title').text).to eq 'Foobar'
-    expect(find('.body').text).to eqe 'Lorem ipsum'
+    expect(find('.title').text).to eq @params[:title]
+    expect(find('.body').text).to eq @params[:body]
   end
 end
