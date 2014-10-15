493211b17411335b9465edfaf3b5b714dc73962d
Michał Skóra <mskora@fractalsoft.org>
2014-10-15 20:18:57 +0200

    Test refactoring

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  fit 'has many images' do
+  it 'has many images' do
     expect(subject).to have_many(:images)
   end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  let(:image) { create(:image) }
-  describe 'belongs to' do
-    fit 'album' do
-      expect(image).to belong_to :album
-    end
+  it 'belongs to album' do
+    expect(subject).to belong_to :album
   end
 end
